16663cd1dd7431e06c1095019ea1c159aca95e8e
nickbaum <nickbaum@gmail.com>
2010-08-13 16:48:21 -0700

    Tweaks to bash script.

--- package.sh
+++ package.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# This script copies all the extension files into a subdirectory
+# removes any files that we don't want in the final crx file,
+# and zips the directory up for easy upload to the gallery
+
 # clean up previous state
 rm -f SimpleWindowSaver.zip;
 rm -rf SimpleWindowSaver;
 
 # copy files
 mkdir SimpleWindowSaver;
-cp * SimpleWindowSaver > /dev/null;
+echo "Expect: cp: SimpleWindowSaver is a directory (not copied)."
+cp * SimpleWindowSaver;
 
 # clean up
 cd SimpleWindowSaver;
-rm -f .DS_Store;
-rm -rf .git;
-rm -f screenshot.png;
-rm -f icon32.png;
-rm -f feedback\ for\ extensions\ team;
-rm -f package.sh;
+files_to_clean_up=(
+	.DS_Store
+	.git
+	feedback
+	icon32.png
+	package.sh
+	screenshot.png
+)
+# TODO: make this safe if the cd fails!
+for i in ${files_to_clean_up[@]}; do
+	rm -rf $i;
+done
 cd ../;
+
+# TODO: add js compilation and html/css minification
 
 # archive
 zip -rq SimpleWindowSaver.zip SimpleWindowSaver;
