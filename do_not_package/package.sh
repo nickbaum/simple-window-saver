66572e89ed3bf916824f10eb1238e79157b69244
nickbaum <nickbaum@gmail.com>
2010-08-18 14:17:01 -0700

    Fixed a bug in opening windows, greatly simplified packaging script.

---
+++ do_not_package/package.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script zips the extension up for easy upload to the gallery
+# It excludes the do_not_package directory as well as .git and .DS_Store
+
+cd ../../
+
+# TODO: add js compilation and html/css minification
+
+# zip for easy upload to the gallery
+zip -rq SimpleWindowSaver SimpleWindowSaver -x \*do_not_package\* \*.DS_Store \*.git\*;
