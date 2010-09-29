2c51d153c390af706ede8c81092b6f4a5b28d6dc
nickbaum <nickbaum@gmail.com>
2010-09-28 19:21:05 -0700

    updated packaging script to match github repo name

--- do_not_package/package.sh
+++ do_not_package/package.sh
@@ -8,4 +8,6 @@ cd ../../
 # TODO: add js compilation and html/css minification
 
 # zip for easy upload to the gallery
-zip -rq SimpleWindowSaver SimpleWindowSaver -x \*do_not_package\* \*.DS_Store \*.git\*;
+zip -rq simple-window-saver simple-window-saver -x \*do_not_package\* \*.DS_Store \*.git\*;
+
+cd -;
