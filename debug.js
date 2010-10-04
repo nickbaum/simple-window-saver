03b29f45d460bd5435976ac8587220b3767cb96c
nickbaum <nickbaum@gmail.com>
2010-10-03 17:57:49 -0700

    added a bunch of comments

--- debug.js
+++ debug.js
@@ -1,28 +1,41 @@
+/*
+
+This file is loaded by the background page.
+It adds a bunch of functionality that is useful for developing the extension.
+
+When the extension is packaged, we replace it with an empty file. You can find this file here: do_not_package/empty_debug.js
+
+Whenever you call a function in this file from the main code, you need to make sure to include a corresponding stub in empty_debug.js to prevent javascript errors.
+
+In general though, we try to keep links between this code and the main code as loose as possible. Currently it is only called in two spots: once from the background page, and once from the popup.
+
+*/
+
+// change the icon so you can distinguish a locally loaded extension
 chrome.browserAction.setIcon({path:'do_not_package/debug_icon19.png'});
 
-// Separate context for debug actions
-debug = {};
+// separate context for debug actions
+// all functions in this file should be in this context
+var debug = {};
 
-// Add link to view logs in popup
-debug.addDebugUI = function(document){
-  var footer = document.getElementById('footer');
-  var url = chrome.extension.getURL("do_not_package/view_log.html");
-  var link = ' - <a href="javascript:chrome.tabs.create({url:\'' + url + '\'});">View logs</a>';
-  footer.innerHTML = footer.innerHTML + link;
-};
-
-// Open database
+// open the debug database
 debug.dbSize = 5 * 1024 * 1024; // 5MB
 debug.db = openDatabase('debug', '1.0', 'debug info', debug.dbSize);
 
-// Create table
+/* LOGGING */
+
+// create a table for log events if it doesn't exist
 debug.db.transaction(function(tx) {
   tx.executeSql('CREATE TABLE IF NOT EXISTS event_log(ID INTEGER PRIMARY KEY ASC, event_type TEXT, window_id INTEGER, tab_id INTEGER, time_stamp DATETIME)', []);
 });
 
-// Log an event
+// by default, we only log events from saved windows
+var onlyLogEventsFromSavedWindows = true;
+
+// log an event
+// we attach this to the various event listeners below
 debug.logEvent = function(eventType, windowId, tabId) {
-  if (windowIdToName[windowId]) {
+  if (onlyLogEventsFromSavedWindows && windowIdToName[windowId]) {
     debug.db.transaction(function(tx){
       var timeStamp = new Date();
       tx.executeSql('INSERT INTO event_log(event_type, window_id, tab_id, time_stamp) VALUES (?,?,?,?)', [eventType, windowId, tabId, timeStamp], null, debug.onError);
@@ -30,12 +43,22 @@ debug.logEvent = function(eventType, windowId, tabId) {
   }
 }
 
-// Log errors to the console
+// log database errors to the console
 debug.onError = function(tx, e) {
   console.log('Something unexpected happened: ' + e.message);
 }
 
-// Log all tab and window events
+// add link to view logs in popup
+debug.addDebugUI = function(document){
+  var footer = document.getElementById('footer');
+  var url = chrome.extension.getURL("do_not_package/view_log.html");
+  var link = ' - <a href="javascript:chrome.tabs.create({url:\'' + url + '\'});">View logs</a>';
+  footer.innerHTML = footer.innerHTML + link;
+};
+
+/* EVENT LISTENERS for logging */
+// To log an event, simply uncomment the corresponding line.
+
 debug.onTabAttached = function(tabId, info) {
   debug.logEvent('tab_attached', info.newWindowId, tabId);
 }
@@ -44,7 +67,7 @@ debug.onTabAttached = function(tabId, info) {
 debug.onTabCreated = function(tab) {
   debug.logEvent('tab_created', tab.windowId, tab.id);
 }
-// chrome.tabs.onCreated.addListener(debug.onTabCreated);
+chrome.tabs.onCreated.addListener(debug.onTabCreated);
 
 debug.onTabDetached = function(tabId, info) {
   debug.logEvent('tab_detached', info.oldWindowId, tabId);
@@ -74,7 +97,7 @@ debug.onTabUpdated = function(tabId, info, tab) {
 debug.onWindowCreated = function(window) {
   debug.logEvent('window_created', window.id, 0);
 }
-// chrome.windows.onCreated.addListener(debug.onWindowCreated);
+chrome.windows.onCreated.addListener(debug.onWindowCreated);
 
 debug.onWindowFocusChanged = function(windowId) {
   debug.logEvent('window_focused', windowId, 0);
--- do_not_package/empty_debug.js
+++ do_not_package/empty_debug.js
@@ -1,3 +1,4 @@
 // In this packaged extension, we do not include debug code.
+// These stubs are only here to avoid javascript errors.
 var debug = {};
 debug.addDebugUI = function(document){};
