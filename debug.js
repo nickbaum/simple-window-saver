46d5d63590d7af103ee87fc141607b87c701bbac
nickbaum <nickbaum@gmail.com>
2010-10-03 17:08:11 -0700

    added link to logs in popup

--- debug.js
+++ debug.js
@@ -1,7 +1,17 @@
-chrome.browserAction.setIcon({path:"do_not_package/debug_icon19.png"});
+chrome.browserAction.setIcon({path:'do_not_package/debug_icon19.png'});
+
+// Separate context for debug actions
+debug = {};
+
+// Add link to view logs in popup
+debug.addDebugUI = function(document){
+	var footer = document.getElementById('footer');
+	var url = chrome.extension.getURL("do_not_package/view_log.html");
+	var link = ' - <a href="javascript:chrome.tabs.create({url:\'' + url + '\'});">View logs</a>';
+	footer.innerHTML = footer.innerHTML + link;
+};
 
 // Open database
-debug = new Object();
 debug.dbSize = 5 * 1024 * 1024; // 5MB
 debug.db = openDatabase('debug', '1.0', 'debug info', debug.dbSize);
 
@@ -27,51 +37,51 @@ debug.onError = function(tx, e) {
 
 // Log all tab and window events
 debug.onTabAttached = function(tabId, info) {
-	debug.logEvent("tab_attached", info.newWindowId, tabId);
+	debug.logEvent('tab_attached', info.newWindowId, tabId);
 }
 // chrome.tabs.onAttached.addListener(debug.onTabAttached);
 
 debug.onTabCreated = function(tab) {
-	debug.logEvent("tab_created", tab.windowId, tab.id);
+	debug.logEvent('tab_created', tab.windowId, tab.id);
 }
 // chrome.tabs.onCreated.addListener(debug.onTabCreated);
 
 debug.onTabDetached = function(tabId, info) {
-	debug.logEvent("tab_detached", info.oldWindowId, tabId);
+	debug.logEvent('tab_detached', info.oldWindowId, tabId);
 }
 // chrome.tabs.onDetached.addListener(debug.onTabDetached);
 
 debug.onTabMoved = function(tabId, info) {
-	debug.logEvent("tab_moved", info.windowId, tabId);
+	debug.logEvent('tab_moved', info.windowId, tabId);
 }
 // chrome.tabs.onMoved.addListener(debug.onTabMoved);
 
 debug.onTabRemoved = function(tabId) {
-	debug.logEvent("tab_removed", tabIdToSavedWindowId[tabId], tabId);
+	debug.logEvent('tab_removed', tabIdToSavedWindowId[tabId], tabId);
 }
 chrome.tabs.onRemoved.addListener(debug.onTabRemoved);
 
 debug.onTabSelectionChanged = function(tabId, info) {
-	debug.logEvent("tab_selected", info.windowId, tabId);
+	debug.logEvent('tab_selected', info.windowId, tabId);
 }
 // chrome.tabs.onSelectionChanged.addListener(debug.onTabSelectionChanged);
 
 debug.onTabUpdated = function(tabId, info, tab) {
-	debug.logEvent("tab_updated", tab.windowId, tabId);
+	debug.logEvent('tab_updated', tab.windowId, tabId);
 }
 // chrome.tabs.onUpdated.addListener(debug.onTabUpdated);
 
 debug.onWindowCreated = function(window) {
-	debug.logEvent("window_created", window.id, 0);
+	debug.logEvent('window_created', window.id, 0);
 }
 // chrome.windows.onCreated.addListener(debug.onWindowCreated);
 
 debug.onWindowFocusChanged = function(windowId) {
-	debug.logEvent("window_focused", windowId, 0);
+	debug.logEvent('window_focused', windowId, 0);
 }
 // chrome.windows.onFocusChanged.addListener(debug.onWindowFocusChanged);
 
 debug.onWindowRemoved = function(windowId) {
-	debug.logEvent("window_removed", windowId, 0);
+	debug.logEvent('window_removed', windowId, 0);
 }
 chrome.windows.onRemoved.addListener(debug.onWindowRemoved);
--- do_not_package/empty_debug.js
+++ do_not_package/empty_debug.js
@@ -1,1 +1,3 @@
 // In this packaged extension, we do not include debug code.
+var debug = {};
+debug.addDebugUI = function(document){};
